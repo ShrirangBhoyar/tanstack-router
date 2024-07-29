9433fb57c27d55733d1254712e465a13473bd369
ShrirangBhoyar <113459541+ShrirangBhoyar@users.noreply.github.com>
2024-07-29 14:53:21 +0530

    starter template in configured in the starter folder in directory

--- starter/src/routeTree.gen.ts
+++ starter/src/routeTree.gen.ts
@@ -1,7 +1,7 @@
 import { FileRoute, lazyFn, lazyRouteComponent } from "@tanstack/react-router"
 
 import { Route as rootRoute } from "./routes/__root"
-import { Route as IndexImport } from "./routes/index"
+import { Route as IndexImport } from "./routes"
 
 const IndexRoute = IndexImport.update({
   path: "/",
